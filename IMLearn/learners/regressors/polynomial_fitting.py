f38e376dbb055e1da9fd0a0741c1e20aa5a3bce5
tomer.gil@mail.huji.ac.il <tomer.gil@mail.huji.ac.il>
2023-04-23 01:27:27 +0300

    Implemented Polynomial Fitting regressor.
     Please enter the commit message for your changes. Lines starting
     with '#' will be ignored, and an empty message aborts the commit.
    
     On branch main
     Your branch is ahead of 'origin/main' by 7 commits.
       (use "git push" to publish your local commits)
    
     Changes to be committed:
    	modified:   IMLearn/learners/regressors/polynomial_fitting.py
    
     Changes not staged for commit:
    	modified:   code examples/Chapter 2 - Linear Regression.ipynb
    	modified:   lab/Lab 00 - B - Data Visualization In Python.ipynb
    	modified:   lab/Lab 02 - Simple Linear Regression.ipynb
    
     Untracked files:
    	my_solutions_to_labs/

--- IMLearn/learners/regressors/polynomial_fitting.py
+++ IMLearn/learners/regressors/polynomial_fitting.py
@@ -19,7 +19,8 @@ class PolynomialFitting(BaseEstimator):
             Degree of polynomial to fit
         """
         super().__init__()
-        raise NotImplementedError()
+        self.degree_ = k
+        self.est_ = LinearRegression()
 
     def _fit(self, X: np.ndarray, y: np.ndarray) -> NoReturn:
         """
@@ -33,7 +34,9 @@ class PolynomialFitting(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        raise NotImplementedError()
+        # vander_matrix = np.vander(X, self.degree_, increasing=True)
+        # self.est_.fit(vander_matrix, y)
+        self.est_.fit(self.__transform(X), y)
 
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
@@ -49,7 +52,13 @@ class PolynomialFitting(BaseEstimator):
         responses : ndarray of shape (n_samples, )
             Predicted responses of given samples
         """
-        raise NotImplementedError()
+        # return np.array(
+        #     [s for sample in np.vander(X, self.degree_, increasing=True)]
+        # )
+
+        # return self.est_.predict(np.vander(X, self.degree_, increasing=True))
+
+        return self.est_.predict(X)
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -68,7 +77,7 @@ class PolynomialFitting(BaseEstimator):
         loss : float
             Performance under MSE loss function
         """
-        raise NotImplementedError()
+        return ((self._predict(X) - y) ** 2).mean()
 
     def __transform(self, X: np.ndarray) -> np.ndarray:
         """
@@ -83,4 +92,4 @@ class PolynomialFitting(BaseEstimator):
         transformed: ndarray of shape (n_samples, k+1)
             Vandermonde matrix of given samples up to degree k
         """
-        raise NotImplementedError()
+        return np.vander(X, self.degree_, increasing=True)
