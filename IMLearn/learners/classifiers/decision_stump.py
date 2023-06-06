2e421f9fd8a985d7b532f87113230be28c2c68c0
tomer <tomer.gil@mail.huji.ac.il>
2023-06-07 00:41:02 +0300

    First version decision stump

--- IMLearn/learners/classifiers/decision_stump.py
+++ IMLearn/learners/classifiers/decision_stump.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import Tuple, NoReturn
-from ...base import BaseEstimator
+from IMLearn.base import BaseEstimator
 import numpy as np
 from itertools import product
 
@@ -39,8 +39,47 @@ class DecisionStump(BaseEstimator):
         y : ndarray of shape (n_samples, )
             Responses of input data to fit to
         """
-        raise NotImplementedError()
+        def get_most_frequent_label(labels: np.ndarray) -> int:
+            values, labels_counts = np.unique(labels, return_counts=True)
+            ind = np.argmax(labels_counts)
+            return values[ind]
 
+        def get_error(labels: np.ndarray, sign: int):
+            return (labels != sign).sum()
+
+        # X = np.c_[X, y]
+        # lowest_error = np.inf
+        # for i in range(X.shape[1]):
+        #     for t in X[:, i].unique():
+        #         # lowers, uppers = X[X[:, i] < t], X[X[:, i] >= t]
+        #         # # _, lowers_labels_counts = np.unique(lowers[:, -1], return_counts=True)
+        #         # # _, uppers_labels_counts = np.unique(uppers[:, -1], return_counts=True)
+        #         # lowers_label, uppers_label = get_most_frequent_label(lowers[:, -1]), \
+        #         #     get_most_frequent_label(uppers[:, -1])
+        #         # err = get_error(lowers[:, -1], lowers_label) + get_error(uppers[:, -1], uppers_label)
+        #
+        #         if err < lowest_error:
+        #             lowest_error = err
+        #             self.j_ = i
+        #             self.threshold_ = t
+        #             self.sign_ =
+        X = np.c_[X, y]
+        for j in range(X.shape[1]):
+            for t in np.unique(X[:, j]):
+                lowers, uppers = X[X[:, j] < t], X[X[:, j] >= t]
+                for sign in [-1, 1]:
+                    err = get_error(lowers[:, -1], sign) + get_error(uppers[:, -1], -sign)
+                    if err < lowest_error:
+                        lowest_error = err
+                        self.j_ = j
+                        self.threshold_ = t
+                        self.sign_ = sign
+
+        # err = np.inf
+        # for j, sign in product(range(X.shape[1]), [-1, 1]):
+        #     thr, thr_err = self._find_threshold(X[:, j], y, sign)
+        #     if thr_err < err:
+        #         self.threshold_, self.j_, self.sign_, err = thr, j, sign, thr_err
     def _predict(self, X: np.ndarray) -> np.ndarray:
         """
         Predict sign responses for given samples using fitted estimator
@@ -95,7 +134,17 @@ class DecisionStump(BaseEstimator):
         For every tested threshold, values strictly below threshold are predicted as `-sign` whereas values
         which equal to or above the threshold are predicted as `sign`
         """
-        raise NotImplementedError()
+        ids = np.argsort(values)
+        values, labels = values[ids], labels[ids]
+
+        # Loss for classifying all as `sign` - namely, if threshold is smaller than values[0]
+        loss = np.sum(np.abs(labels)[np.sign(labels) == sign])
+
+        # Loss of classifying threshold being each of the values given
+        loss = np.append(loss, loss - np.cumsum(labels * sign))
+
+        id = np.argmin(loss)
+        return np.concatenate([[-np.inf], values[1:], [np.inf]])[id], loss[id]
 
     def _loss(self, X: np.ndarray, y: np.ndarray) -> float:
         """
@@ -115,3 +164,9 @@ class DecisionStump(BaseEstimator):
             Performance under missclassification loss function
         """
         raise NotImplementedError()
+
+d = DecisionStump()
+# d.fit(np.random.randn(20, 3), np.random.randint(-1, 2, 20))
+# d.fit(np.random.randn(20, 3), np.array([1, 1, -1, 1, -1, -1, 1, 1, -1, 1, -1, -1, 1, 1, -1, 1, -1, -1, 1, -1]))
+d.fit(np.random.randn(5, 3), np.array([1, 1, -1, -1, 1]))
+print()
